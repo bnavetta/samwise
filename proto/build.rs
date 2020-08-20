d91072e09ea902a5680b18cf7e8e13c266d78811
Ben Navetta <ben.navetta@gmail.com>
2020-08-19 21:44:03 -0700

    Fine, install rustfmt

--- proto/build.rs
+++ proto/build.rs
@@ -1,9 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("src/samwise.proto")?;
-
-    tonic_build::configure()
-        .format(false)
-        .compile(&["src/samwise.proto"], &["src"])?;
-
     Ok(())
 }
