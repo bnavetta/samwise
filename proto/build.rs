cd35b80a778d2ba98e0f348ff492af90833d70c4
Ben Navetta <ben.navetta@gmail.com>
2020-08-19 21:34:35 -0700

    Don't require rustfmt to build

--- proto/build.rs
+++ proto/build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("src/samwise.proto")?;
+
+    tonic_build::configure()
+        .format(false)
+        .compile(&["src/samwise.proto"], &["src"])?;
+
     Ok(())
 }
